8706c31280537300212e2e7fba672f28066fa3a6
asdf1899 <araid.anas99@gmail.com>
2017-12-15 17:31:08 +0100

    Add some section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // script per aprire e chiudere la sidebar
 function apriMenu() {
     document.getElementById("menuLat").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
+    document.getElementById("opacita").style.display = "block";
 }
 
 function chiudiMenu() {
     document.getElementById("menuLat").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
+    document.getElementById("opacita").style.display = "none";
 }
