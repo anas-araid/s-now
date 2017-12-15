d00410e98a4b4c8d6ad3dea27c7fe09102558d86
asdf1899 <araid.anas99@gmail.com>
2017-12-15 15:48:11 +0100

    Add app layout

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// script per aprire e chiudere la sidebar
+function apriMenu() {
+    document.getElementById("menuLat").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function chiudiMenu() {
+    document.getElementById("menuLat").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
