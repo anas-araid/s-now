5fd4aef6728258bb1268dee42909ec69d74a2110
asdf1899 <araid.anas99@gmail.com>
2018-01-07 20:02:44 +0100

    Add gallery section

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ function chiudiMenu() {
     document.getElementById("menuLat").style.display = "none";
     document.getElementById("opacita").style.display = "none";
 }
+var indice=0;
+function slideImmagini(){
+  var img = document.getElementById("galleria");
+  img.src = "img/img" + indice + ".jpg";
+  indice += 1;
+  if (indice === 5){
+    indice = 0;
+  }
+  setTimeout(slideImmagini, 2000);
+}
