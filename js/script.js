03facfa1a77a088c7f600000980ddbc79bd691ae
asdf1899 <araid.anas99@gmail.com>
2018-01-07 21:09:20 +0100

    Change mobile nav bar color

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 function apriMenu() {
     document.getElementById("menuLat").style.display = "block";
     document.getElementById("opacita").style.display = "block";
+    document.getElementById("mapdiv").style.display = "none"; // la mappa sovrapponeva la sidebar 
 }
 
 function chiudiMenu() {
     document.getElementById("menuLat").style.display = "none";
     document.getElementById("opacita").style.display = "none";
+    document.getElementById("mapdiv").style.display = "block";
 }
 var indice=0;
 function slideImmagini(){
