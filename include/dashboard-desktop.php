b8d150e390a66654977dd5e340f5c8671fb67ee3
asdf1899 <araid.anas99@gmail.com>
2018-04-27 18:23:58 +0200

    Add google maps api key

--- include/dashboard-desktop.php
+++ include/dashboard-desktop.php
@@ -68,37 +68,9 @@
       </h2>
       <hr class="stile-azzurro" style="width:100px;height:8px;border:5px solid white;border-radius:10px;">
 
-      <!-- don't send HTTP referer for privacy purpose and to use Google Maps Geocoding API without key -->
-      <meta name="referrer" content="no-referrer">
       <div>
-        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgwoQUpZNuWrgKJseSI53sQvWZAFkBzQ4&callback=initMap" type="text/javascript"></script>
         <script type="text/javascript">
-        // ################################################################################################
-        // Script trovato su internet per bypassare il controllo sulla chiave api per google mpas
-        var target = document.head;
-        var observer = new MutationObserver(function(mutations) {
-            for (var i = 0; mutations[i]; ++i) { // notify when script to hack is added in HTML head
-                if (mutations[i].addedNodes[0].nodeName == "SCRIPT" && mutations[i].addedNodes[0].src.match(/\/AuthenticationService.Authenticate?/g)) {
-                    var str = mutations[i].addedNodes[0].src.match(/[?&]callback=.*[&$]/g);
-                    if (str) {
-                        if (str[0][str[0].length - 1] == '&') {
-                            str = str[0].substring(10, str[0].length - 1);
-                        } else {
-                            str = str[0].substring(10);
-                        }
-                        var split = str.split(".");
-                        var object = split[0];
-                        var method = split[1];
-                        window[object][method] = null; // remove censorship message function _xdc_._jmzdv6 (AJAX callback name "_jmzdv6" differs depending on URL)
-                        //window[object] = {}; // when we removed the complete object _xdc_, Google Maps tiles did not load when we moved the map with the mouse (no problem with OpenStreetMap)
-                    }
-                    observer.disconnect();
-                }
-            }
-        });
-        var config = { attributes: true, childList: true, characterData: true }
-        observer.observe(target, config);
-        //######################################################################################################
         var initialize = function() {
           <?php
             $coordResidenza = getCoordinatesFromAddress($user['Residenza'], "Italia")
