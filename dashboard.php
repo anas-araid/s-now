09f839e3b036715a87687096ba3c24d9b4615d45
asdf1899 <araid.anas99@gmail.com>
2018-08-02 21:43:04 +0200

    Add chat section to mobile view, add a modal that contain all messages

--- dashboard.php
+++ dashboard.php
@@ -90,7 +90,7 @@
 
             <div class="mdl-card mdl-cell mdl-cell--4-col mdl-shadow--4dp mdl-color--white stile-card-corners ">
               <h2 class="stile-text-azzurro ">
-                Chat
+                Chat recenti
               </h2>
                <hr class="stile-azzurro" style="width:100px;height:8px;border:5px solid white;border-radius:10px;">
                <div style="text-align:center">
@@ -98,12 +98,21 @@
                          style="width:90%;height:35px;color:white;background-color:#27ae60;border:none;border-radius:20px;;text-align:center;margin-bottom:15px"
                          onclick="location.href='chat.php?username='">
                    Scrivi a un segnalatore
+                   <i class="material-icons">account_circle</i>
+                 </button>
+                 <button class="mdl-button mdl-js-button mdl-button--raised"
+                         style="width:90%;height:35px;color:white;background-color:#27ae60;border:none;border-radius:20px;;text-align:center;margin-bottom:15px;margin-bottom:2xp"
+                         onclick="modal.open();">
+                   Tutti i messaggi
                    <i class="material-icons">chat</i>
                  </button>
                </div>
                <div id="lastMsg" style="overflow:auto">
-                 <?php include "php/get_last_messages.php" ?>
-               <div>
+                 <?php
+                    include "php/get_last_messages.php";
+                    getMessages($user, "LIMIT 2");
+                 ?>
+               </div>
             </div>
           </div>
         </section>
@@ -123,6 +132,32 @@
         document.getElementById("loading-div").remove();
         document.getElementById("main").style.display = "block";
       }, 2000);
+
+
+      // modal per la visualizzazione di tutti i messaggi
+      var modal = new tingle.modal({
+          closeMethods: ['overlay', 'button', 'escape'],
+          closeLabel: "Chiudi",
+          cssClass: ['custom-class-1', 'custom-class-2'],
+          onOpen: function() {
+              console.log('modal open');
+          },
+          onClose: function() {
+              console.log('modal closed');
+          },
+          beforeClose: function() {
+              return true; // close the modal
+              return false; // nothing happens
+          }
+      });
+      var messages = `<?php getMessages($user, ''); ?>`;
+      // set content
+      modal.setContent(
+        '<h2 class="stile-text-azzurro">Tutti i messaggi<h2>'+
+        "<div>"+
+        messages +
+        "</div>"
+      );
     </script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgwoQUpZNuWrgKJseSI53sQvWZAFkBzQ4&callback=initMap" type="text/javascript"></script>
   </body>
--- php/get_last_messages.php
+++ php/get_last_messages.php
@@ -1,20 +1,23 @@
 <?php
-  include "php/db_connection.php";
-  $msgQuery = "SELECT * FROM t_messaggi WHERE (FK_Mittente = '".$user['ID']."') or (FK_Destinatario ='".$user['ID']."') ORDER BY ID DESC LIMIT 2";
-  $getMsg = mysqli_query($db_conn, $msgQuery);
-  if ($getMsg != null){
-    while ($ris = mysqli_fetch_array($getMsg)){
-      $senderID = $ris['FK_Mittente'];
-      $receiverID = $ris['FK_Destinatario'];
-      $senderData = getMessagingData($senderID, $db_conn);
-      $receiverData = getMessagingData($receiverID, $db_conn);
+  function getMessages($user, $clause){
+    include "php/db_connection.php";
+    //user è  $user = getUserData($_SESSION['email'], "php/db_connection.php");
+    $msgQuery = "SELECT * FROM t_messaggi WHERE (FK_Mittente = '".$user['ID']."') or (FK_Destinatario ='".$user['ID']."') ORDER BY ID DESC $clause";
+    $getMsg = mysqli_query($db_conn, $msgQuery);
+    if ($getMsg != null){
+      while ($ris = mysqli_fetch_array($getMsg)){
+        $senderID = $ris['FK_Mittente'];
+        $receiverID = $ris['FK_Destinatario'];
+        $senderData = getMessagingData($senderID, $db_conn);
+        $receiverData = getMessagingData($receiverID, $db_conn);
 
-      echo '
-      <div class=" mdl-cell mdl-cell--8-col mdl-shadow--4dp mdl-cell-middle mdl-color--white stile-card-corners" style="width:80%">
-      <a href="chat.php?username='.$senderData['Email'].'" style="color:#27ae60!important">'.$senderData['Nome'].': </a>
-      <p>'.$ris['Messaggio'].'</p>
-      <p class="stile-text-azzurro" style="display:inline!important">Inviato il '.date('d-m-Y', strtotime($ris['Data'])).' a '.$receiverData['Nome'].'</p>
-      </div>';
+        echo '
+        <div class=" mdl-cell mdl-cell--8-col mdl-shadow--4dp mdl-cell-middle mdl-color--white stile-card-corners" style="width:80%">
+        <a href="chat.php?username='.$senderData['Email'].'" style="color:#27ae60!important;font-size:20px!important;">'.$senderData['Nome'].': </a>
+        <p>'.$ris['Messaggio'].'</p>
+        <p class="stile-text-azzurro" style="display:inline!important">Inviato il '.date('d-m-Y', strtotime($ris['Data'])).' a '.$receiverData['Nome'].'</p>
+        </div>';
+      }
     }
   }
   function getMessagingData($id, $db_conn){
